698d3b42947140baa991f58af505fb94d04fe2f9
vaishnavi reddy <vaishnavikallam4@gmail.com>
2026-08-22 15:22:32 +0530

    Update program.c

--- 2520030198_Practical/Practical-01/src/program.c
+++ 2520030198_Practical/Practical-01/src/program.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+char  buff[100];
+int pid;
+printf("Enter linux commond:");
+scanf("%s",buff);
+pid=fork();
+if(pid<0)
+{
+printf("Fork failed\n");
+}
+if(pid==0)
+{
+printf("Child process\n");
+printf("Child PID: %d\n", getpid());
+printf("Parent PID: %d\n", getppid());
+execlp(buff,buff,NULL);
+printf("Buffer execution failed\n");
+}
+else
+{
+printf("Parent Process\n");
+printf("Parent PID: %d\n", getpid());
+printf("Child PID: %d\n", pid);
+printf("Child process completed\n");
+}
+}
 
