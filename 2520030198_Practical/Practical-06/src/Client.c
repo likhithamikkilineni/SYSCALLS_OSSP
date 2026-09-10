af1d837fc41019e01417949460eb60a34a6699bc
vaishnavi reddy <vaishnavikallam4@gmail.com>
2026-09-10 09:18:30 +0530

    Implement client-server message communication
    Added functionality to send a message to the server and receive a response.

--- 2520030198_Practical/Practical-06/src/Client.c
+++ 2520030198_Practical/Practical-06/src/Client.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+int main()
+{
+char message[100];
+char response[100];
+int fd1, fd2;
+printf("Enter message: ");
+fgets(message, sizeof(message), stdin);
+fd1 = open("server_fifo", O_WRONLY);
+write(fd1, message, strlen(message) + 1);
+close(fd1);
+fd2 = open("client_fifo", O_RDONLY); // reponse from server
+read(fd2, response, sizeof(response));
+printf("Server says: %s\n", response);
+close(fd2);
+ return 0;
+}
