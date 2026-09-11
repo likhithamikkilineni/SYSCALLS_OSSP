3b18111d04a36bfa7a8ba4d224287a9f834bb5f7
vaishnavi reddy <vaishnavikallam4@gmail.com>
2026-09-11 20:07:23 +0530

    Implement signal handling in signal.c
    Added signal handling for SIGINT, SIGTERM, and SIGUSR1.

--- 2520030198_Practical/Practical-06/src/signal.c
+++ 2520030198_Practical/Practical-06/src/signal.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+void handle_signal(int sig)
+{
+if (sig == SIGINT)
+printf("\nSIGINT received!\n");
+else if (sig == SIGTERM)
+ printf("\nSIGTERM received!\n");
+else if (sig == SIGUSR1)
+printf("\nSIGUSR1 received!\n");
+}
+int main()
+{
+signal(SIGINT, handle_signal);
+signal(SIGTERM, handle_signal);
+signal(SIGUSR1, handle_signal);
+printf("Program started...\n");
+printf("Process ID: %d\n", getpid());
+while (1)
+{
+printf("Program is running...\n");
+sleep(3);
+}
+}
 
