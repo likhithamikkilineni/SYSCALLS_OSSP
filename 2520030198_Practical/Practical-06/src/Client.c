28d0721d3746f5ff97c668472b17931e656dc9e1
vaishnavi reddy <vaishnavikallam4@gmail.com>
2026-09-11 19:55:29 +0530

    Fix message writing in Client.c for buffer safety
    Updated message writing to use sizeof instead of strlen for buffer safety.

--- 2520030198_Practical/Practical-06/src/Client.c
+++ 2520030198_Practical/Practical-06/src/Client.c
@@ -2,16 +2,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <string.h>
 int main()
 {
 char message[100];
 char response[100];
 int fd1, fd2;
 printf("Enter message: ");
-fgets(message, sizeof(message), stdin);
+fgets(message, sizeof(message), stdin); // reads msg from user (keyboard)
 fd1 = open("server_fifo", O_WRONLY);
-write(fd1, message, strlen(message) + 1);
+write(fd1, message, sizeof(message));
 close(fd1);
 fd2 = open("client_fifo", O_RDONLY); // reponse from server
 read(fd2, response, sizeof(response));
